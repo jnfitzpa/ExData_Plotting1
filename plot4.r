7b4717a98a9478306d7c9fdc7fc697c8c2835580
jnfitzpa <jnfitzpa@iupui.edu>
2017-10-15 15:09:23 -0400

    Add files via upload

--- plot4.r
+++ plot4.r
@@ -7,12 +7,14 @@ dataset$DtTm = as.POSIXct(paste(dataset$Date, dataset$Time), format="%Y-%m-%d %H
 
 par(mfrow = c(2,2))
 with(dataset,{
+    plot(Global_active_power ~ DtTm,type = "l",xlab = "", ylab='Global Active Power')
+    plot(Voltage ~ DtTm,type = "l",xlab = "datetime", ylab='Voltage')
     plot(Sub_metering_1 ~ DtTm,type = "l",xlab = "", ylab='Energy sub metering')
     lines(Sub_metering_2 ~ DtTm,col="red")
     lines(Sub_metering_3 ~ DtTm,col="blue")
     legend("topright",col = c("black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty = c(1,1,1))
+    plot(Global_reactive_power ~ DtTm,type = "l",xlab = "datetime", ylab='Global_reactive_power')
 })
 
-
-dev.copy(png,file="plot3.png")
+dev.copy(png,file="plot4.png")
 dev.off()
